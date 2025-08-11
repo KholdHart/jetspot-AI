21b72375fb6c362e70363c7ca39ff93f4a611c29
Spojta <166438476+Spojta@users.noreply.github.com>
2025-08-11 13:39:11 +0200

    installed imgaekit and express libs and also tested

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
 import express from "express";
 
+
 const port = process.env.PORT || 3000;
+const app = express();  // this was missing
+
+app.get("/api/upload", (req, res) => {
+  res.send("It works");
+});
+
+app.listen(port, () => {
+  
+  console.log(`Server running on port ${port}`);
+});
